38099c9d450b5fd3c3805f99ae5828f9cb9bbf7c
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-07-05 20:46:38 +0530

    init
    init

---
+++ app/components/GsapTo.js
@@ -0,0 +1,25 @@
+const GsapTo = () => {
+  return (
+    <main>
+      <h1>GsapTo</h1>
+
+      <p className="mt-5 text-gray-500">
+        The <code>gsap.to()</code> method is used to animate elements from their
+        current state to a new state.
+      </p>
+      <p className="mt-5 text-gray-500">
+        The <code>gsap.to()</code> method is similar to the{" "}
+        <code>gsap.from()</code> method, but the difference is that the{" "}
+        <code>gsap.to()</code> method animates elements from their current state
+        to a new state, while the <code>gsap.from()</code> method animates
+        elements from a new state to their current state.
+      </p>
+
+      <div className="mt-20">
+        <div id="blue-box" className="w-20 h-20 bg-blue-500 rounded-lg" />
+      </div>
+    </main>
+  );
+};
+
+export default GsapTo;
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GsapTo from './components/GsapTo'
+
+const page = () => {
+  return (
+    <>
+    <GsapTo/>
+    </>
+  )
+}
+
+export default page
