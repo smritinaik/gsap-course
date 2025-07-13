2e5aecec4d3838f7d193de6d4fefeac29812cbd5
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-07-13 21:25:34 +0530

    gsap scroll trigger
    gsap scroll trigger

--- app/components/GsapScrollTrigger.js
+++ app/components/GsapScrollTrigger.js
@@ -14,7 +14,7 @@ const GsapScrollTrigger = () => {
 
         boxes.forEach((box) => {
             gsap.to(box , {
-                x:150 * (boxes.indexOf(box) + 2),
+                x:150 ,
                 rotation: 360,
                 borderRadius:'100%',
                 scale:1.5,
