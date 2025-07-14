0073ba9946220d0657bbebfbceb98b56d3884d0e
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-07-14 20:37:18 +0530

    gsap text effect
    gsap text effect

--- app/page.js
+++ app/page.js
@@ -4,10 +4,13 @@ import GsapFrom from './components/Gsapfrom'
 import GsapFromTo from './components/GsapFromTo'
 import GsapStagger from './components/t GsapStagger '
 import GsapScrollTrigger from './components/GsapScrollTrigger'
+import GsapText from './components/GsapTextEffect'
 
 const page = () => {
   return (
     <>
+    <GsapText/>
+    
     <GsapTo/>
     <GsapFrom/>
     <GsapFromTo/>
